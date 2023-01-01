d7b04b5ca98f1c47641a14680bbef582cb9f447b
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2023-01-01 15:27:46 -0500

    home page implemented along with footer

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      © Kruger Corp. 2022. Transforming Enterprises, Boosting Lives.
+    </div>
+  );
 };
 
 export default Footer;

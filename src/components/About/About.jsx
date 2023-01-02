eb0fdff35156a736bbae760261e3630667361103
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2023-01-01 19:37:56 -0500

    about me developed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import "./About.scss";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <div className="about__container">
+        <div className="about__container-img">
+          <img src="./pepe.jpg" alt="" />
+        </div>
+        <div className="about__container-info">
+          <div className="about__container-info__name">
+            Jose Andres Gavilanes
+          </div>
+          <div className="about__container-info__description">
+            Hi, I’m a Full-Stack Developer and Mechatronics Engineer, bla bla
+            bla Hi, I’m a Full-Stack Developer and Mechatronics Engineer, bla
+            bla bla Hi, I’m a Full-Stack Developer and Mechatronics Engineer,
+            bla bla bla Hi, I’m a Full-Stack Developer and Mechatronics
+            Engineer, bla bla bla
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
